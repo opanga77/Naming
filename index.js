d36bf5ee69925dc4148aaa2b1eb2698008a7927f
opanga77 <andersoking77@gmail.com>
2020-02-14 18:09:09 +0300

    commit

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturda
             document.getElementById('message').innerHTML = "<div class=\"alert alert-danger\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">x</button><strong>Morio!</strong><em>Your submision is invalid</em>!</div>";
             $('#message').addClass("animated shake");
         }
+        else {
+            for(var i=0;i<myGender.length;i++){
+                if(myGender[i].checked){
+                    if(myGender[i].value === "Male"){
+                        document.getElementById('message').innerHTML = "<span><i class=\"fa fa-male\"></i></span>&nbsp;&nbsp; Yes! Got it!... Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + maleAkanNames[dayOfTheWeek] + "</span>";
+                        $('#message span:first-child').addClass("animated fadeInDown");
+                        $('#message span:last-child').addClass("animated fadeInUp");
+                    }
